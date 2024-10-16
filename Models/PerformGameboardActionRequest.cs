c559ae1cc8dfe297499f748d82ac1810afe1c50f
Jaari <jaari.rey@gmail.com>
2024-10-16 12:39:02 +1000

    added Required annotations for PerformGameboardActionRequest properties

--- Models/PerformGameboardActionRequest.cs
+++ Models/PerformGameboardActionRequest.cs
@@ -1,10 +1,14 @@
 ﻿using Chess.Classes;
+using System.ComponentModel.DataAnnotations;
 
 namespace ChessAPI.Models
 {
     public class PerformGameboardActionRequest
     {
+        [Required(ErrorMessage = "Gameboard is required.")]
         public Gameboard Gameboard { get; set; }
+
+        [Required(ErrorMessage = "Action is required.")]
         public Action Action { get; set; }
     }
 }
