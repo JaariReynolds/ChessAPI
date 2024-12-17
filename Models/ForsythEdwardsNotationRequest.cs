e38a649eb3051486a400d239319407d3d4d33e17
jaari <jaari.rey@gmail.com>
2024-12-17 18:54:51 +0800

    removed whitespace line

--- Models/ForsythEdwardsNotationRequest.cs
+++ Models/ForsythEdwardsNotationRequest.cs
@@ -3,7 +3,6 @@
 namespace ChessAPI.Models
 {
     public class ForsythEdwardsNotationRequest
-
     {
         [Required(ErrorMessage = "Forsyth-Edwards Notation (FEN) string is required.")]
         public string FenString { get; set; }
